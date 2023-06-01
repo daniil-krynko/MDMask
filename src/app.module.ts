5eb1d7bdc5e87d66fe71cff7c3a6af9021d8ecb1
Daniil Krynko <daniil.krynko@gmail.com>
2023-06-01 13:58:57 +0300

    Added user and photo entities, mongodb connenction

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { PhotosModule } from './photos/photos.module';
+import { MongooseModule } from "@nestjs/mongoose";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true}),
+    UsersModule,
+    PhotosModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
